c1a2e66948d657f32552bcb9de3a1ffef80d1b86
Ashley Sands <ashsands1@amstrike.com>
2018-12-23 01:53:56 +0000

    added CueSelect button

---
+++ Plugins/ActionCue_editor/Source/ActionCue_editor/Public/CueSelectButton.h
@@ -0,0 +1,15 @@
+// Ashley M Sands (gizzmo123456) 2018
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class ACTIONCUE_EDITOR_API CueSelectButton
+{
+public:
+	CueSelectButton();
+	~CueSelectButton();
+};
